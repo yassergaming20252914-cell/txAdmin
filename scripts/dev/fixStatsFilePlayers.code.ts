d2d9322a6d166bd0beb318e6685a11a27c3fc3b9
yassergaming20252914-cell <yassergaming20252914@gmail.com>
2025-09-02 21:15:50 +0200

    Update fixStatsFilePlayers.code.ts
    DEVLOPER YASSER

--- scripts/dev/fixStatsFilePlayers.code.ts
+++ scripts/dev/fixStatsFilePlayers.code.ts
@@ -35,3 +35,13 @@ export default (sourceFileName: string, targetFileName: string) => {
 
     fs.writeFileSync(targetFileName, JSON.stringify(targetFileData));
 }
+/**
+ * Inject txTracker data (hourlyPlayers30d) into the stats_svRuntime.json file
+ * 
+ * NOTE: RENAME TO fixStatsFilePlayers.local.ts
+ * NOTE: THEN RUN npx tsx scripts/dev/fixStatsFilePlayers.local.ts
+ */
+import fixStatsFilePlayers from "./fixStatsFilePlayers.code";
+const sourceFileName = 'xxxxxx/txtracker/poc/xxxxx-hourlyPlayers30d.json'
+const targetFileName = 'xxxxxx/txData/default/data/stats_svRuntime.json'
+fixStatsFilePlayers(sourceFileName, targetFileName);
